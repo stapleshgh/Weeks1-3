96a70f9da5c1a554ced9ba7e3bfdee6e6b644db4
staples <howardhickey11@gmail.com>
2025-01-26 21:35:02 -0500

    added taro arm swinging dance

--- Assets/mechanical drawing/scripts/taro.cs
+++ Assets/mechanical drawing/scripts/taro.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class taro : MonoBehaviour
 {
     public AnimationCurve curve;
+    //curves for right arm1, right arm 2, left arm 1 and left arm 2
+    public AnimationCurve RAC1;
+    public AnimationCurve RAC2;
+    public AnimationCurve LAC1;
+    public AnimationCurve LAC2;
     float time = 0f;
 
     //child object references. grabs the arm pivots from the scene
@@ -24,6 +29,7 @@ public class taro : MonoBehaviour
     void Update()
     {
         bounceSprite();
+        taroDance();
     }
 
     void bounceSprite()
@@ -36,6 +42,14 @@ public class taro : MonoBehaviour
         {
             time = 0f;
         }
+    }
+
+    void taroDance()
+    {
+        rightArmPivot1.transform.localRotation = Quaternion.Euler(0, 0, RAC1.Evaluate(time));
+        rightArmPivot2.transform.localRotation = Quaternion.Euler(0, 0, RAC2.Evaluate(time));
+        leftArmPivot1.transform.localRotation = Quaternion.Euler(0, 0, LAC1.Evaluate(time));
+        leftArmPivot2.transform.localRotation = Quaternion.Euler(0, 0, LAC2.Evaluate(time));
     }
     
 }
